6f03722652b5e34d9fbd0b339cf42429806f21c1
Selmar <selmarpellizzoni@yahoo.com.br>
2024-11-25 12:08:57 -0300

    Create uTrataExcecao.pas

---
+++ Fontes/Servidor/uTrataExcecao.pas
@@ -0,0 +1,36 @@
+unit uTrataExcecao;
+
+interface
+
+uses
+  System.SysUtils, uTrataExcecaoPai;
+
+type
+
+  TTrataExcecao = class(TTratarExcecaoPai)
+  public
+    constructor Create; reintroduce;
+  end;
+
+implementation
+
+uses
+  Vcl.Forms;
+
+{ TTrataExcecao }
+
+constructor TTrataExcecao.Create;
+begin
+  inherited Create(True);
+end;
+
+var
+  FTratarExcecao : TTratarExcecaoPai;
+
+initialization;
+  FTratarExcecao := TTrataExcecao.Create;
+
+finalization;
+  FTratarExcecao.Free;
+
+end.
