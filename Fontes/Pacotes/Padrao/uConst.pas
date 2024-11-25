f616c3a20e5d22222e508030aa647bb33bd78b52
Selmar <selmarpellizzoni@yahoo.com.br>
2024-11-25 12:08:14 -0300

    Create uConst.pas

---
+++ Fontes/Pacotes/Padrao/uConst.pas
@@ -0,0 +1,14 @@
+unit uConst;
+
+interface
+
+const
+  cGetCep = '/GETCEP';
+  cGetCeps = '/GETCEPS';
+  cPostCep = '/POSTCEP';
+  cPutCep = '/PUTCEP';
+  cDeleteCep = '/DELETE';
+
+implementation
+
+end.
